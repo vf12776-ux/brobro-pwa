6326196563eee5480d5416a5190366cca7fd07c1
Вадим Фомин <vadimfomin@MacBook-Air-Vadim.local>
2026-09-20 18:22:51 +0200

    v10: full adaptation from nastoechnaya template to brobro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'brobro-v5';
+const CACHE_NAME = 'brobro-v10';
 const ASSETS = [
   './',
   './index.html',
@@ -6,16 +6,25 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
