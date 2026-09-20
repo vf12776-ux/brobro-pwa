d1c91a3eeacd3251080ad1d0e85e93e4e022c244
Вадим Фомин <vadimfomin@MacBook-Air-Vadim.local>
2026-09-20 17:54:35 +0200

    v5: add nojekyll and fix sw/manifest

--- sw.js
+++ sw.js
@@ -1,40 +1,21 @@
-const CACHE_NAME = 'brobro-v4';
-const urlsToCache = [
+const CACHE_NAME = 'brobro-v5';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
-  self.clients.claim();
 });
